1d63a1952ddcb6ce380b47ebf078a836df4ba615
root <473075311@qq.com>
2021-04-01 17:25:53 +0800

    master修改了hello

--- src/main/java/com/turing/sp5/test/Hello.java
+++ src/main/java/com/turing/sp5/test/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
     public static  void  main(String[] args ){
 
         System.out.print(456);
-        System.out.print("master");
+        System.out.print("master1");
     }
 }
